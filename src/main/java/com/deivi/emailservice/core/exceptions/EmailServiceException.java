b1da3e5294ba0a6659e8090250321087002fe24c
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-11-03 18:03:22 -0300

    add EmailServiceException

---
+++ src/main/java/com/deivi/emailservice/core/exceptions/EmailServiceException.java
@@ -0,0 +1,11 @@
+package com.deivi.emailservice.core.exceptions;
+
+public class EmailServiceException extends RuntimeException {
+    public EmailServiceException(String message) {
+        super(message);
+    }
+
+    public EmailServiceException(String massage, Throwable cause) {
+        super(massage, cause);
+    }
+}
