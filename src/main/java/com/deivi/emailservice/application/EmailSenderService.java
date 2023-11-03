b6152fb2765c6a78d4a00b0ca11670f4dece5caf
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-11-03 17:55:30 -0300

    refactor EmailSenderService (service annotation)

--- src/main/java/com/deivi/emailservice/application/EmailSenderService.java
+++ src/main/java/com/deivi/emailservice/application/EmailSenderService.java
@@ -1,10 +1,12 @@
 package com.deivi.emailservice.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.deivi.emailservice.adapters.EmailSenderGateway;
 import com.deivi.emailservice.core.EmailSenderUserCase;
 
+@Service
 public class EmailSenderService implements EmailSenderUserCase{
 
     private final EmailSenderGateway emailSenderGateway;
